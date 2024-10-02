1c9d7cac28950acd96ef9ea2e02ec60ef0392c51
Екатерина <diada.var@mail.ru>
2024-10-02 08:20:50 +0300

    hw

---
+++ src/main/java/pro/sky/stringEmploeerProekt/EmployeeStorageIsFullException.java
@@ -0,0 +1,27 @@
+package pro.sky.stringEmploeerProekt;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class EmployeeStorageIsFullException extends RuntimeException {
+    public EmployeeStorageIsFullException() {
+    }
+
+    public EmployeeStorageIsFullException(String message) {
+        super(message);
+    }
+
+    public EmployeeStorageIsFullException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public EmployeeStorageIsFullException(Throwable cause) {
+        super(cause);
+    }
+
+    public EmployeeStorageIsFullException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+}
