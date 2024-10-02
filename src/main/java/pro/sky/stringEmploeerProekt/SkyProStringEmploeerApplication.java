3f384f953d7d9a8a5dad0726db0eb49411e92622
Екатерина <diada.var@mail.ru>
2024-10-02 07:12:26 +0300

    start

---
+++ src/main/java/pro/sky/stringEmploeerProekt/SkyProStringEmploeerApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.stringEmploeerProekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProStringEmploeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyProStringEmploeerApplication.class, args);
+	}
+
+}
